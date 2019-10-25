db0057bc8df7a631ea66e87bdaad09fac40bc45a
ksator <khelil.sator.git@gmail.com>
2019-10-25 11:52:00 -0700

    add files to configure network

--- configure_the_network.py
+++ configure_the_network.py
@@ -1,31 +1,42 @@
-# generate config and load config (dry_run) on a single device  
+
+# generate config files and load config files on network devices 
 
 from nornir import InitNornir
 from nornir.plugins.tasks import networking, text
 from nornir.plugins.functions.text import print_title, print_result
+from nornir.core.filter import F
 
 def configuration(task):
     r = task.run(task=text.template_file, name="Generate configuration", template="config.j2", path=f"templates/{task.host.platform}")
     task.host["config"] = r.result
     task.run(task=networking.napalm_configure, name="Loading configuration on the device", replace=False, configuration=task.host["config"])
 
-nr = InitNornir(config_file="config.yaml", dry_run=True)
-dev = nr.filter(name="vMX1")
-# dev.inventory.hosts
-
-# from nornir.core.filter import F
-# Junos_devices = nr.filter(F(platform="junos"))
+nr = InitNornir(config_file="config.yaml", dry_run=False)
+Junos_devices = nr.filter(F(platform="junos"))
 
 print_title("Playbook to configure the network")
-result = dev.run(task=configuration)
+result = Junos_devices.run(task=configuration)
 print_result(result)
+
 
 '''
 # to check if a task failed:
 result.failed
-result["vMX1"].failed
-result["vMX1"][2]
-result["vMX1"][2].failed
 '''
 
+'''
+# to check if the device vMX1 configuration changed
+result["vMX1"].changed
+result["vMX1"][2]
+result["vMX1"][2].changed
+result["vMX1"][2].diff
+'''
+
+'''
+# the check which devices had a configuration changed 
+for item in Junos_devices.inventory.hosts:
+   dev=nr.inventory.hosts[item]
+   print ('**** ' + item + ' ****')
+   result[item][2].changed 
+'''
 
