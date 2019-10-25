cd5753fe073dbf06dc952fc252e6f8fe6df3b5f6
ksator <khelil.sator.git@gmail.com>
2019-10-25 06:28:37 -0700

    add tasks examples

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-This repo has nornir content for Junos automation.  
+This repo has Nornir content for Junos automation.  
 
 Visit the repository [wiki](https://github.com/ksator/junos_automation_with_nornir/wiki) to get the instructions 
 
---
+++ execute_tasks_against_ubuntu.py
@@ -0,0 +1,15 @@
+from nornir import InitNornir
+from nornir.plugins.tasks import commands
+from nornir.plugins.functions.text import print_result
+
+def check_ubuntu(task):
+    task.run(task=commands.remote_command, name="Ubuntu release", command="lsb_release -a")
+    task.run(task=commands.remote_command, name="Available disk", command="df -h")
+    task.run(task=commands.remote_command, name="Available memory", command="free -m")
+    task.run(task=commands.remote_command, name="Ping google public ipv4 dns", command="ping 8.8.8.8 -c 3")
+
+nr = InitNornir(config_file="config.yaml")
+paris_servers = nr.filter(site="paris", type="server")
+result = paris_servers.run(task=check_ubuntu)
+print_result(result, vars=["stdout"])
+
